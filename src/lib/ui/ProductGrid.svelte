64f10083bd3ddee353ddbb08205dc8d2e7e815c6
maxitect <maxime.downe@gmail.com>
2025-03-13 14:27:23 +0000

    chore: add ProductGrid.svelte component

---
+++ src/lib/ui/ProductGrid.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import ProductCard, { type ProductProps } from './ProductCard.svelte';
+
+	interface Props {
+		products: ProductProps[];
+	}
+	let { products }: Props = $props();
+</script>
+
+<div
+	class="bg-dark grid grid-cols-2 gap-x-4 gap-y-6 p-6 pt-6 pb-12 md:gap-x-6 md:gap-y-10 xl:grid-cols-4 xl:gap-6"
+>
+	{#each products as p}
+		<ProductCard name={p?.name} handle={p?.handle} price={p?.price} images={p?.images} />
+	{/each}
+</div>
