01068c842193547b28f32421d498acc13907926c
maxitect <maxime.downe@gmail.com>
2025-03-13 12:58:34 +0000

    chore: create ProductCard.svelte

--- src/lib/ui/ProductCard.svelte
+++ src/lib/ui/ProductCard.svelte
@@ -1,3 +1,20 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		handle: string;
+		price: number;
+		images: string[];
+	}
+	let { title, handle, price, images }: Props = $props();
+</script>
+
 <div>
-    
+	<a
+		href="/products/{handle}"
+		class="bg-light-container-mid flex w-full flex-col justify-center gap-2 rounded-4xl px-3 py-4"
+	>
+		<img src={images[0]} alt="{title} product image" />
+		<h3 class="text-dark">{title}</h3>
+		<p class="text-dark">£{price}</p>
+	</a>
 </div>
