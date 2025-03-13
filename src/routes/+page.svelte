9afa0a75c476d950c7d9724d5b5ef1903c608288
maxitect <maxime.downe@gmail.com>
2025-03-13 14:41:14 +0000

    chore: add styling and main page description in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,6 @@
 	import products from '$lib/data/products.json';
 </script>
 
-<h1 class="text-dark text-center text-4xl">Got Soap?</h1>
+<h1 class="text-dark p-10 text-center text-4xl">Got Soap?</h1>
+<p class="mb-6">Check out our handcrafted, all natural selection of soaps and shampoo bars.</p>
 <ProductGrid {products} />
