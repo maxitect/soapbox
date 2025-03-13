ae222361eec180f511093feefe8518ccd77b6894
maxitect <maxime.downe@gmail.com>
2025-03-13 14:40:25 +0000

    style: remove margins from ProductGrid.svelte as this can be handled in layout

--- src/lib/ui/ProductGrid.svelte
+++ src/lib/ui/ProductGrid.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div
-	class="bg-dark grid grid-cols-2 gap-x-4 gap-y-6 p-6 pt-6 pb-12 md:gap-x-6 md:gap-y-10 xl:grid-cols-4 xl:gap-6"
+	class="bg-dark grid grid-cols-2 gap-x-4 gap-y-6 md:gap-x-6 md:gap-y-10 xl:grid-cols-4 xl:gap-6"
 >
 	{#each products as p}
 		<ProductCard name={p?.name} handle={p?.handle} price={p?.price} images={p?.images} />
