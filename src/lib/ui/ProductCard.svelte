eb1b9b63dae2bf05a0124406174fbfc4ae1378ab
maxitect <maxime.downe@gmail.com>
2025-03-13 16:12:27 +0000

    style: add hover effect on ProductCard.svelte

--- src/lib/ui/ProductCard.svelte
+++ src/lib/ui/ProductCard.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
-	export interface ProductProps {
-		name: string;
-		handle: string;
-		price: number;
-		images: string[];
-	}
+	import type { ProductProps } from '$lib/types';
 	let { name, handle, price, images }: ProductProps = $props();
 </script>
 
 <div>
 	<a
 		href="/products/{handle}"
-		class="bg-light-container-mid flex w-full flex-col justify-center gap-2 border-2"
+		class="bg-light-container-mid flex w-full flex-col justify-center gap-2 overflow-hidden border-2"
 	>
-		<img src={images[0]} alt="{name} product image" class="m-0 p-0" />
+		<img src={images[0]} alt="{name} product image" class="m-0 p-0 hover:scale-110" />
 		<div class="flex justify-between p-6 text-xl">
 			<h3 class="text-dark text-bold">{name}</h3>
 			<p class="text-dark">£{price}</p>
