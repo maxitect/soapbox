9816dda2eea29862f8eb91f6980415a2f194a430
maxitect <maxime.downe@gmail.com>
2025-03-13 12:38:38 +0000

    chore: initial Header.svelte component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,7 @@
+<div
+	class="border-color-dark sticky top-0 flex h-24 w-full items-center justify-between border-b bg-white pr-12 pl-4"
+>
+	<a href="/">
+		<h1 class="text-2xl font-bold text-red-500">Soapbox</h1>
+	</a>
+</div>
