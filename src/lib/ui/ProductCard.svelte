f5b01537301f4fa86f28dbdb01708dd37b95bc0a
maxitect <maxime.downe@gmail.com>
2025-03-13 14:27:03 +0000

    chore: add ProductCard.svelte component

--- src/lib/ui/ProductCard.svelte
+++ src/lib/ui/ProductCard.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
-	interface Props {
-		title: string;
+	export interface ProductProps {
+		name: string;
 		handle: string;
 		price: number;
 		images: string[];
 	}
-	let { title, handle, price, images }: Props = $props();
+	let { name, handle, price, images }: ProductProps = $props();
 </script>
 
 <div>
 	<a
 		href="/products/{handle}"
-		class="bg-light-container-mid flex w-full flex-col justify-center gap-2 rounded-4xl px-3 py-4"
+		class="bg-light-container-mid flex w-full flex-col justify-center gap-2 border-2"
 	>
-		<img src={images[0]} alt="{title} product image" />
-		<h3 class="text-dark">{title}</h3>
-		<p class="text-dark">£{price}</p>
+		<img src={images[0]} alt="{name} product image" class="m-0 p-0" />
+		<div class="flex justify-between p-6 text-xl">
+			<h3 class="text-dark text-bold">{name}</h3>
+			<p class="text-dark">£{price}</p>
+		</div>
 	</a>
 </div>
